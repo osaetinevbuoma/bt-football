c417453741809de669216bf8b3af2a871f2cde8f
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2022-02-04 08:54:15 +0000

    Displayed processed data in view

--- test/services/bt_football_service_test.dart
+++ test/services/bt_football_service_test.dart
@@ -1,3 +1,5 @@
+import 'package:bt_football/models/match.dart';
+import 'package:bt_football/models/team.dart';
 import 'package:bt_football/services/bt_football_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -6,10 +8,35 @@ void main() {
     test('Fetch matches based on duration of the last 30 days', () async {
       final service = BTFootballService();
       List<Match> matches =
-          (await service.fetchMatches('2022-01-03', '2022-02-03'))
+          (await service.fetchMatches('2022-01-04', '2022-02-03'))
               .cast<Match>();
 
-      // expect(matches.length, matcher)
+      expect(matches.isNotEmpty, true);
+    });
+
+    test('Determine team with most wins', () async {
+      final service = BTFootballService();
+      List<Match> matches =
+          (await service.fetchMatches('2022-01-04', '2022-02-03'))
+              .cast<Match>();
+      Team team = service.determineTeamWithMostWins(matches);
+
+      expect(team.name.isNotEmpty, true);
+      expect(team.founded?.isNaN, false);
+      expect(team.wins.isNaN, false);
+      expect(team.losses.isNaN, false);
+      expect(team.draws.isNaN, false);
+    });
+
+    test('Fetch team squad details', () async {
+      final service = BTFootballService();
+      Team team = await service.fetchTeam(57);
+
+      expect(team.name.isNotEmpty, true);
+      expect(team.name.isNotEmpty, true);
+      expect(team.clubColors?.isNotEmpty, true);
+      expect(team.founded?.isNaN, false);
+      expect(team.squad?.isNotEmpty, true);
     });
   });
 }
